c845d0de4e8432901d8cb552372637e7fc247fa0
Augusto215 <augusto.dataanalysis@gmail.com>
2024-08-16 00:41:46 -0300

    novo12

---
+++ link_checkout_figen.js
@@ -0,0 +1,11 @@
+
+
+// ALTERE O LINK CORRESPONTE AO POTE
+// produto_checkout('PROD_POTE_12', 'https://www.link_do_pote_12.com');
+
+produto_checkout('PROD_POTE_12', 'https://ev.braip.com/campanhas/cpa/camxm69eo');
+produto_checkout('PROD_POTE_6', 'https://ev.braip.com/campanhas/cpa/cam6l9xom');
+produto_checkout('PROD_POTE_3', 'https://ev.braip.com/campanhas/cpa/camzpve6x');
+produto_checkout('PROD_POTE_1', 'https://ev.braip.com/campanhas/cpa/cam26jxo1');
+
+page_figen_pdv('https://jornalsaudebrasil.site/pvfigen01','https://jornalsaudebrasil.site/pvfigen01');
